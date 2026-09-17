6862e436c03ad54c8201739446fade87352d479c
hunil58 <hunil58@gmail.com>
2026-09-17 15:51:04 +0900

    feat: 메인 메뉴 화면 UI 구성 및 MainMenuController 구현
    MainMenu 씬이 완전히 비어 있어 Camera/EventSystem/Canvas부터 새로
    만들었다. 새 회차 시작/이어하기/종료 버튼과 저장 덮어쓰기 확인
    다이얼로그를 두고, MainMenuController가 SaveManager.HasSave로
    이어하기 활성/비활성을 판정하고 GameManager로 씬 전환을 위임한다.
    
    TMP 한글 폰트 아틀라스(NanumGothicSDF)는 이번에 쓴 글자들로
    런타임에 채워져 같이 커밋한다.
    
    이슈 #90

---
+++ Assets/Scripts/Runtime/UI/MainMenuController.cs
@@ -0,0 +1,88 @@
+using NCAIClicker.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NCAIClicker.UI
+{
+    /// <summary>
+    /// MainMenu 씬의 새 회차 시작/이어하기/종료 버튼을 GameManager로 위임한다.
+    /// 씬 전환은 GameManager가 조정하며 이 컨트롤러는 SceneManager를 직접 부르지 않는다 (이슈 #90).
+    /// 저장 존재 여부는 SaveManager.Instance.HasSave(이슈 #139)로 판정한다.
+    /// </summary>
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private Button _newRunButton;
+        [SerializeField] private Button _continueButton;
+        [SerializeField] private Button _quitButton;
+        [SerializeField] private GameObject _overwriteConfirmPanel;
+        [SerializeField] private Button _overwriteConfirmYesButton;
+        [SerializeField] private Button _overwriteConfirmNoButton;
+
+        private void Awake()
+        {
+            if (_overwriteConfirmPanel != null)
+            {
+                _overwriteConfirmPanel.SetActive(false);
+            }
+        }
+
+        private void OnEnable()
+        {
+            RefreshContinueButton();
+
+            _newRunButton.onClick.AddListener(HandleNewRunClicked);
+            _continueButton.onClick.AddListener(HandleContinueClicked);
+            _quitButton.onClick.AddListener(HandleQuitClicked);
+            _overwriteConfirmYesButton.onClick.AddListener(HandleOverwriteConfirmed);
+            _overwriteConfirmNoButton.onClick.AddListener(HandleOverwriteCanceled);
+        }
+
+        private void OnDisable()
+        {
+            _newRunButton.onClick.RemoveListener(HandleNewRunClicked);
+            _continueButton.onClick.RemoveListener(HandleContinueClicked);
+            _quitButton.onClick.RemoveListener(HandleQuitClicked);
+            _overwriteConfirmYesButton.onClick.RemoveListener(HandleOverwriteConfirmed);
+            _overwriteConfirmNoButton.onClick.RemoveListener(HandleOverwriteCanceled);
+        }
+
+        private void RefreshContinueButton()
+        {
+            var saveService = SaveManager.Instance;
+            _continueButton.interactable = saveService != null && saveService.HasSave;
+        }
+
+        private void HandleNewRunClicked()
+        {
+            var saveService = SaveManager.Instance;
+            if (saveService != null && saveService.HasSave)
+            {
+                _overwriteConfirmPanel.SetActive(true);
+                return;
+            }
+
+            GameManager.Instance?.StartNewRun();
+        }
+
+        private void HandleContinueClicked()
+        {
+            GameManager.Instance?.ContinueRun();
+        }
+
+        private void HandleQuitClicked()
+        {
+            GameManager.Instance?.QuitGame();
+        }
+
+        private void HandleOverwriteConfirmed()
+        {
+            _overwriteConfirmPanel.SetActive(false);
+            GameManager.Instance?.StartNewRun();
+        }
+
+        private void HandleOverwriteCanceled()
+        {
+            _overwriteConfirmPanel.SetActive(false);
+        }
+    }
+}
